c19996a1a481aba4d9cd8cd35daa2aecc3020ac5
Gagan <gaganichake@yahoo.com>
2017-10-04 18:22:55 -0500

    Initial commit

---
+++ src/gagan/practice/ftp/Main.java
@@ -0,0 +1,26 @@
+package gagan.practice.ftp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws Throwable 
+	 */
+	public static void main(String[] args) throws Throwable {
+
+		List<String> fileNamesToSend = new ArrayList<String>();
+		fileNamesToSend.add("C:\\Users\\Gagan\\Downloads\\IDBI Salary Overdraft Facility.pdf");
+		fileNamesToSend.add("C:\\Users\\Gagan\\Downloads\\IMG_1182.JPG");
+		//FTPUtility.sendFiles(fileNamesToSend);
+		
+		List<String> fileNamesToRemove = new ArrayList<String>();
+		fileNamesToRemove.add("IDBI Salary Overdraft Facility.pdf");
+		fileNamesToRemove.add("MG_1182.JPG");
+		FTPUtility.removeFiles(fileNamesToRemove);
+	}
+	
+}
+
